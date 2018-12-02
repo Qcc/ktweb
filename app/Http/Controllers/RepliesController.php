3b5d5d20cc416c70e06682ba3f75a71fb907c93f
kevin_notepad <kevin.e@qq.com>
2018-12-02 19:55:59 +0800

    解决了执行超过30秒问题，邮件扩展问题

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -6,6 +6,7 @@ use App\Models\Reply;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReplyRequest;
+use Illuminate\Support\Facades\Log;
 use Auth;
 
 class RepliesController extends Controller
@@ -26,6 +27,7 @@ class RepliesController extends Controller
 		$reply->user_id = Auth::id();
 		$reply->topic_id = $request->topic_id;
 		$reply->save();
+		Log::info('RepliesController-> store');
 		return redirect()->to($reply->topic->link())->with('success', '回复成功！');
 	}
 
