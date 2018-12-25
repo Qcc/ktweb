5dac21d454e43361da5387797588176871826555
vagrant <vagrant@homestead>
2018-12-25 09:42:59 +0800

    合并部分JS

--- public/js/app.js
+++ public/js/app.js
@@ -15,6 +15,157 @@ $$(document).ready(function () {
 		});
 	}
 
+	// 底部客户提交信息模块
+	$$("#customer").on("click", function () {
+		$$(this).attr('disabled', true);
+		var name = $$('#name').val();
+		var phone = $$('#phone').val();
+		$$.ajax({
+			method: 'POST',
+			url: '/business/store',
+			ContentType: 'application/json',
+			headers: {
+				'X-CSRF-TOKEN': $$('meta[name="csrf-token"]').attr('content')
+			},
+			data: {
+				name: name,
+				phone: phone,
+				type: '终端客户',
+			},
+			success: function (data) {
+				var data = JSON.parse(data);
+				if (data.status) {
+					$$('.form').css('visibility', 'hidden');
+					$$('.msg').text(data.msg);
+					$$('.success').show();
+				} else {
+					$$('.fiald').text(data.msg);
+				}
+				$$('#customer').removeAttr('disabled');
+			}
+		})
+	});
+	// 底部客户提交信息模块结束
+
+	if ($$('.business-info-page').length == 1) {
+		//合作伙伴提交信息
+		$$("#partner").on("click", function () {
+			$$(this).attr('disabled', true);
+			var name = $$('#name').val();
+			var phone = $$('#phone').val();
+			var city = $$('#city').val();
+			$$.ajax({
+				method: 'POST',
+				url: '/business/store',
+				ContentType: 'application/json',
+				headers: {
+					'X-CSRF-TOKEN': $$('meta[name="csrf-token"]').attr('content')
+				},
+				data: {
+					name: name,
+					phone: phone,
+					city: city,
+					type: '代理商',
+				},
+				success: function (data) {
+					var data = JSON.parse(data);
+					if (data.status) {
+						$$('.msg').text(data.msg);
+						$$('.success').show();
+					} else {
+						$$('.fiald').text(data.msg);
+					}
+					$$('#partner').removeAttr('disabled');
+				}
+			});
+		});
+	}
+
+
+
+	// 首页动画
+	if ($$('.home-page').length == 1) {
+		// body 动画
+		var ctbsRun = true,
+			kingdeeRun = true,
+			yzjRun = true,
+			jdyRun = true
+		enterprisesRun = true,
+			usersRun = true;
+		// body 动画结束
+		// header部分
+		$$(document).ready(function () {
+			// 添加导航菜单背景色
+			$$(document).on("scroll", function () {
+				runNumberAnimat(); // 添加body内容数字动画
+			});
+			// 初始化首页轮播图
+			if ($$('.swiper-container').length === 1) {
+				var swiper = new Swiper('.swiper-container', {});
+				// 初始化元素可视运行动画
+				wow = new WOW({
+					animateClass: 'animated',
+				});
+				wow.init();
+
+				// 执行数字动画
+				runNumberAnimat();
+			}
+		});
+
+		// 检查元素是否可见
+		function isVisible(element, winScrollTop) {
+			var mainOffsetTop = $$("#" + element).offset().top;
+			var mainHeight = $$("#" + element).height();
+			var winHeight = $$(window).height();
+			if (winScrollTop > mainOffsetTop + mainHeight || winScrollTop < mainOffsetTop - winHeight) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		// 启动数字动画
+		function runNumberAnimat() {
+			var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+			if (isVisible('ctbs', scrollTop) && ctbsRun) {
+				// 初始化数字动态动画
+				var ctbs = new CountUp('ctbs', 1, 10000, 0, 3);
+				ctbs.start();
+				ctbsRun = false;
+			}
+			if (isVisible('kingdee', scrollTop) && kingdeeRun) {
+				// 初始化数字动态动画
+				var kingdee = new CountUp('kingdee', 1, 70, 0, 3);
+				kingdee.start();
+				kingdeeRun = false;
+			}
+			if (isVisible('yzj', scrollTop) && yzjRun) {
+				// 初始化数字动态动画
+				var yzj = new CountUp('yzj', 1, 99, 0, 3);
+				yzj.start();
+				yzjRun = false;
+			}
+			if (isVisible('jdy', scrollTop) && jdyRun) {
+				// 初始化数字动态动画
+				var jdy = new CountUp('jdy', 1, 80, 0, 3);
+				jdy.start();
+				jdyRun = false;
+			}
+			if (isVisible('enterprises', scrollTop) && enterprisesRun) {
+				// 初始化数字动态动画
+				var enterprises = new CountUp('enterprises', 1, 10000, 0, 3);
+				enterprises.start();
+				enterprisesRun = false;
+			}
+			if (isVisible('users', scrollTop) && usersRun) {
+				// 初始化数字动态动画
+				var users = new CountUp('users', 1, 100000, 0, 3);
+				users.start();
+				usersRun = false;
+			}
+		}
+	}
 	// 展开导航菜单
 	$$('.kt-products').on('click', function () {
 		$$('.kt-menu-tab-head i').removeClass('active');
