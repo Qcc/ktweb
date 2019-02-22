1da532d6b143d3be3f526242e6b3905bffb9d543
vagrant <vagrant@homestead>
2019-02-22 14:31:04 +0800

    增加百度商桥

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <!-- CSRF Token -->
@@ -9,14 +10,39 @@
     <meta name="description" content="@yield('description', '沟通科技CTBS、云桌面RAS远程接入软件提供移动办公,专业虚拟打印,移动虚拟化,金蝶用友ERP的远程接入解决方案和Citrix替代方案,企业管理软件及ERP云服务商,包括金蝶云服务、云ERP管理软件、云之家移动办公、精斗云财务软件、财务共享，企业上云，行业解决方案等')" />
     <meta name="keywords" content="@yield('keywords', '沟通科技,沟通,CTBS,远程接入,应用虚拟化,金蝶,金蝶云,数字化转型,新零售,SaaS,erp管理系统,财务软件,移动办公,云erp,KIS,企业上云,K/3,K/3 Clould')" />
     <title>@yield('title') - 深圳市沟通科技有限公司</title>
+    <!-- 百度商桥 kouton.com -->
+    @if(strstr(route('home'),'kouton.com'))
+    <script>
+        var _hmt = _hmt || [];
+        (function () {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?d88fee424125e31fa06120bae4eaf78a";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+    @endif
+    <!-- 百度商桥 kingdeecn.com -->
+    @if(strstr(route('home'),'kingdeecn.com'))
+    <script>
+        var _hmt = _hmt || [];
+        (function () {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?9d05c3c2dd0103ec6ba6c263a37b3ca2";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+    @endif
     <link rel="stylesheet" href="{{ asset('layui/css/layui.css') }}">
     <link rel="stylesheet" href="{{ asset('css/mdui.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/swiper.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @yield('styles')
-    
+
 </head>
+
 <body class="mdui-theme-primary-indigo mdui-theme-accent-indigo">
     <div class="{{ route_class() }}-page">
         @include('layouts._header')
@@ -24,20 +50,21 @@
             @include('layouts._message')
             @yield('content')
             <button class="mdui-fab mdui-fab-fixed mdui-color-theme-accent reset-top mdui-ripple"><i class="kticon">&#xe786;</i></button>
-        </div>    
+        </div>
         @include('layouts._footer')
     </div>
-  <script src="{{ asset('js/jquery-1.9.1.min.js') }}"></script>
-  <script src="{{ asset('layui/layui.js') }}"></script>
-  <script src="{{ asset('js/mdui.min.js') }}"></script>
-  <script src="{{ asset('js/swiper.min.js') }}"></script>
-  <script src="{{ asset('js/countUp.min.js') }}"></script>
-  <script src="{{ asset('js/wow.min.js') }}"></script>
-  <script src="//at.alicdn.com/t/font_916960_dsr6fh19j3f.js"></script>
-  <script src="{{ asset('js/app.js') }}"></script>
-  @if (app()->isLocal())
-        @include('sudosu::user-selector')
+    <script src="{{ asset('js/jquery-1.9.1.min.js') }}"></script>
+    <script src="{{ asset('layui/layui.js') }}"></script>
+    <script src="{{ asset('js/mdui.min.js') }}"></script>
+    <script src="{{ asset('js/swiper.min.js') }}"></script>
+    <script src="{{ asset('js/countUp.min.js') }}"></script>
+    <script src="{{ asset('js/wow.min.js') }}"></script>
+    <script src="//at.alicdn.com/t/font_916960_dsr6fh19j3f.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    @if (app()->isLocal())
+    @include('sudosu::user-selector')
     @endif
-  @yield('script')
+    @yield('script')
 </body>
+
 </html>
