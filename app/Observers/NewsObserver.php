9c56a27e39b68333057256c5126512e224904fe4
kevin <kevin.e@qq.com>
2019-04-28 11:49:04 +0800

    style 新闻组

--- app/Observers/NewsObserver.php
+++ app/Observers/NewsObserver.php
@@ -20,7 +20,7 @@ class NewsObserver
         
         //excerpt 字段存储的是话题的摘录，将作为文章页面的 description 元标签使用
         //make_excerpt() 是自定义的辅助方法，我们需要在 helpers.php 文件中添加
-        $news->excerpt = make_excerpt($news->body);
+        $news->excerpt = trim(make_excerpt($news->body));
     }
 
     public function saved(News $news)
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -38,7 +38,7 @@ class TopicObserver
         
         //excerpt 字段存储的是话题的摘录，将作为文章页面的 description 元标签使用
         //make_excerpt() 是自定义的辅助方法，我们需要在 helpers.php 文件中添加
-        $topic->excerpt = make_excerpt($topic->body);
+        $topic->excerpt = trim(make_excerpt($topic->body));
     }
 
     public function saved(Topic $topic)
